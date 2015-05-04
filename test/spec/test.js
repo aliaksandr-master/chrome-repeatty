335849f1112fb408cce291011925b685051ced3a
aliaksandr-pasynkau <alxe.master@gmail.com>
2015-05-04 13:25:57 +0300

    initial commit

---
+++ test/spec/test.js
@@ -0,0 +1,12 @@
+'use strict';
+/* global describe, it */
+
+(function () {
+	describe('Give it some context', function () {
+		describe('maybe a bit more context here', function () {
+			it('should run here few assertions', function () {
+
+			});
+		});
+	});
+})();
