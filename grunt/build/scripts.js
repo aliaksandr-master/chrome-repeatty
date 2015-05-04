ee9542a29a892f90f93b92908eede10dc90cd757
aliaksandr-pasynkau <alxe.master@gmail.com>
2015-05-04 15:00:56 +0300

    add babel and jsx support

--- grunt/build/scripts.js
+++ grunt/build/scripts.js
@@ -9,6 +9,70 @@ module.exports = function (grunt, CFG) {
 			CFG.SRC + '/scripts/**/*.js'
 		])
 
+		.react({
+			options: {
+				sourceMap: true,
+				es6module: true,
+				nonStrictEs6module: true
+			},
+			files: [
+				{
+					expand: true,
+					cwd: CFG.SRC,
+					src: [
+						'**/*.jsx',
+						'!bower_components/**/*.jsx'
+					],
+					dest: CFG.BUILD,
+					ext: '.jsx'
+				}
+			]
+		})
+
+		.babel('scriptsWithModules', {
+			options: {
+				sourceMap: true//,
+				//modules: 'none'
+			},
+			files: [
+				{
+					expand: true,
+					cwd: CFG.SRC,
+					src: [
+						'**/*.js',
+						'**/*.jsx',
+						'!bower_components/**/*.jsx',
+						'!bower_components/**/*.js',
+						'!require.js'
+					],
+					dest: CFG.BUILD,
+					ext: '.js'
+				}
+			]
+		})
+
+		.babel('scriptsWithoutModules', {
+			options: {
+				sourceMap: true,
+				modules: 'common'
+			},
+			files: [
+				{
+					expand: true,
+					cwd: CFG.SRC,
+					src: [
+						'require.js'
+					],
+					dest: CFG.BUILD,
+					ext: '.js'
+				}
+			]
+		})
+
+		.clean([
+			CFG.BUILD + '/**/*.jsx'
+		])
+
 		.concat({
 			options: {
 				stripBanners: true
